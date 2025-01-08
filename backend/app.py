ecac495d7321044f42e4639a75d06abf666aaa27
srt03 <139922324+srtaps@users.noreply.github.com>
2025-01-08 03:33:49 +0100

    Set up project dependencies, add code for DB connection

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import pymysql.cursors
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt, jwt_required
+
+# Connection to database
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='your_password',
+    database='your_db_name',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+# Create the cursor, keep it open
+cursor = connection.cursor()
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*":{'origins':"*"}})
+# Allow requests from Vue
+CORS(app)
 
 @app.get("/")
 def home():
