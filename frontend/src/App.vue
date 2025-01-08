74d0af88ae6c8a881e398e30e73c87494847fb71
srt03 <139922324+srtaps@users.noreply.github.com>
2025-01-08 22:10:24 +0100

    Remove unecessary files, edit css and login view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <h1>App.vue</h1>
   <RouterView />
 </template>
 
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Login test</h1>
+</template>
