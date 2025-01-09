2df84334704cabe10c7805a80d964c12acae5aaf
srt03 <139922324+srtaps@users.noreply.github.com>
2025-01-10 00:13:59 +0100

    Remove register route to implement differently

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/views/Login.vue";
-import Register from "@/views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: "/login",
       name: "Login",
       component: Login,
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: Register,
     },
   ],
 });
