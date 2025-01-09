6712e4993097893ec63f69fba126f0422dae9712
srt03 <139922324+srtaps@users.noreply.github.com>
2025-01-09 23:27:23 +0100

    Add register view, template structure, and route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/login",
       name: "Login",
       component: Login,
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: Register,
     },
   ],
 });
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <form action="" class="form" id="register-form">
+      <h2 class="form__title"></h2>
+      <label for="first-name">First name</label>
+      <input type="text" id="first-name" name="firstName" required />
+      <label for="last-name">Last name</label>
+      <input type="text" id="last-name" name="lastName" required />
+      <label for="first-name">Email</label>
+      <input type="email" id="email" name="email" required />
+      <label for="first-name">Password</label>
+      <input type="password" id="password" name="password" required />
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
