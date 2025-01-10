5d09916c3ede66c34f200406f1483b767d7f09a4
srt03 <139922324+srtaps@users.noreply.github.com>
2025-01-10 02:02:21 +0100

    Edit heading text, add links

--- frontend/src/components/AuthCard.vue
+++ frontend/src/components/AuthCard.vue
@@ -2,7 +2,17 @@
   <div class="auth-card">
     <div class="container">
       <div class="auth-card__left">
-        <h2>{{ heading }}</h2>
+        <h2 class="auth-card__title">{{ heading }}</h2>
+        <p v-if="formType === 'register'">
+          Already have an account?<br /><a href="/login"
+            >Click here to log in</a
+          >
+        </p>
+        <p v-else>
+          Don't have an account?<br /><a href="/register"
+            >Click here to sign up</a
+          >
+        </p>
       </div>
       <div class="auth-card__right">
         <!-- Dynamically load the form based on the formType prop -->
@@ -25,7 +35,9 @@ export default {
   },
   computed: {
     heading() {
-      return this.formType === "register" ? "Register" : "Login";
+      return this.formType === "register"
+        ? "Join our community"
+        : "Welcome to VueLearn";
     },
     formComponent() {
       // Lazy loading components
