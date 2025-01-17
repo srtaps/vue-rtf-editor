fd2a5108e1141c86fc8cd636933f3091455457f8
srt03 <139922324+srtaps@users.noreply.github.com>
2025-01-17 01:32:54 +0100

    Add new route for viewing Users

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AuthCard from "@/components/AuthCard.vue";
 import Home from "@/views/Home.vue";
 import LessonEditor from "@/views/LessonEditor.vue";
+import Users from "@/views/Users.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,12 @@ const router = createRouter({
       path: "/new-lesson",
       name: "Editor",
       component: LessonEditor,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/users",
+      name: "Users",
+      component: Users,
       meta: { requiresAuth: true },
     },
   ],
---
+++ frontend/src/views/Users.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="vue-views" id="users">
+    <Navbar />
+    <div class="vue-views__container">
+      <h2>Users</h2>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+</script>
